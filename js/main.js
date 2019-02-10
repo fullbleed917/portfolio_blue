dae09eef54c8d5445b7e012baae2eddeaffac489
Leon Redman <leonproart@yahoo.com>
2019-02-09 22:31:24 -0700

    project update css and javascript

--- js/main.js
+++ js/main.js
@@ -13,6 +13,10 @@
     }
   }
 
+
+
+
+
 $( document ).ready(function() {
     console.log( "ready!" );
     $( ".burger-nav" ).click(function() {
@@ -27,8 +31,8 @@ $( document ).ready(function() {
 
 $(function() {
     var header = $(".header");
-  
-    $(window).scroll(function() {    
+
+    $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         if (scroll >= 50) {
             header.addClass("scrolled");
@@ -36,10 +40,24 @@ $(function() {
             header.removeClass("scrolled");
         }
     });
-  
+
 });
 
 
+var myIndex = 0;
+carousel();
 
 
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
 
+      setTimeout(carousel, 5000);
+
+};
